32524d540b31731f6b71f3776b9919731dee5803
turtlebot <turtlebot@TURTLEBOOK1.(none)>
2017-02-15 10:31:45 -0600

    gave default parameter

--- nodes/gray_transceiver_port_node.py
+++ nodes/gray_transceiver_port_node.py
@@ -16,7 +16,7 @@ from gray_transceiver_message import *
 
 METATOPICNAME = rospy.get_param("/gray_transceiver/metatopic_name","/gray_transceiver/metatopic")
 
-json = rospy.get_param("/throughput_test/json_bool")
+json = rospy.get_param("/throughput_test/json_bool", True)
 
 
 class gray_transceiver_port(object):
